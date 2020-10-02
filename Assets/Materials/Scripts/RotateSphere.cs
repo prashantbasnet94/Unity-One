9548d87f76702f9a3886cfcb86389b1701d8207b
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2020-10-02 09:53:23 -0500

    first and third rotates around the 2nd one

---
+++ Assets/Materials/Scripts/RotateSphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSphere : MonoBehaviour
+{
+    Vector3 movement;
+    public int xi, yi, zi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(xi, yi, zi);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement* Time.deltaTime);
+    }
+}
